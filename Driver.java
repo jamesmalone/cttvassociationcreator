d8b78ee3222a205e4cc1fc499ec7c0914d8ff8da
james.malone@gmail.com <james.malone@gmail.com>
2014-11-24 14:33:25 +0000

    whitespace error checking and sourceDB added

--- Driver.java
+++ Driver.java
@@ -13,7 +13,7 @@ public class Driver {
         //input file, output file
         //parser.parseFile("/Users/malone/Dropbox/CTTV/text mining work/ibd work/Jatin IBD feedback/curated_IBD_JatinPatelreview.txt", "file:/Users/malone/ibd_associations_27_oct.owl");
 
-        parser.parseFile("/Users/malone/Dropbox/CTTV/common to rare/hpo_ORDO_141104_tab.txt", "file:/Users/malone/Dropbox/CTTV/common%20to%20rare/ordo_hpo_mappings.owl");
+        parser.parseFile("/Users/malone/Dropbox/CTTV/common to rare/hpo_ORDO_141104_tab.txt", "file:/Users/malone/Dropbox/CTTV/common%20to%20rare/ordo_hpo_mappings2.owl");
 
 //"file:/Users/malone/Dropbox/CTTV/text mining work/ibd work/Jatin IBD feedback/ibd_associations_27_oct.owl"
     }
